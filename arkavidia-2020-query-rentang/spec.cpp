053a371b07bec3ee21aaadef40b19cc1961d0452
Irfan Sofyana Putra <irfansofyana0305@gmail.com>
2019-12-25 21:06:28 +0700

    update statement

--- arkavidia-2020-query-rentang/spec.cpp
+++ arkavidia-2020-query-rentang/spec.cpp
@@ -3,24 +3,35 @@
 using namespace std;
 using namespace tcframe;
 
+const int MAXN = 1000000;
+const int MAXM = 2000;
+
 class ProblemSpec : public BaseProblemSpec{
     protected:
         //list of variables
+        int N, Q;
+        vector<int> L, R;
+        vector<int> ql, qr;
+        vector<int> ans;
 
     void InputFormat(){
-
+        LINE(N);
+        LINES(L, R) % SIZE(N);
+        LINE(Q);
+        LINES(ql, qr) % SIZE(Q);
     }
 
     void OutputFormat(){
-
+        LINES(ans) % SIZE(Q);
     }
 
     void GradingConfig(){
-
+        TimeLimit(1);
+        MemoryLimit(32);
     }
 
     void Constraints(){
-        
+        CONS(1 <= N && N <= )
     }
 };
 
