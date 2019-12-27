fdae49148e91f625b2589303bbfb52f2c24ae6d3
Michel Fang <mfang555@gmail.com>
2019-12-27 13:09:25 +0800

    (triple-phytagoras): update spec.cpp

--- arkavidia-2020-triple-phytagoras/spec.cpp
+++ arkavidia-2020-triple-phytagoras/spec.cpp
@@ -40,7 +40,8 @@ protected:
     vector<int> primes;
     int sz_prime;
     vector<bool> isPrime;
-    map<int, vector<ll>> bigPrimes; // primes with digit >= 7
+    map<int, vector<ll>> bigPrimes; // primes with digit >= 7. baru digit 7 - 10, 17-18
+    map<ll, bool> vis;
 
     void SampleTestCase1() {
         Input({
@@ -48,15 +49,6 @@ protected:
         });
         Output({
             "1"
-        });
-    }
-
-    void SampleTestCase2() {
-        Input({
-            "1"
-        });
-        Output({
-            "0"
         });
     }
 
@@ -95,12 +87,43 @@ protected:
             258863287,
             877582847
         };
-        bigPrimes[17] = {
-            17070066725738647
+        bigPrimes[10] = {
+            6265877659,
+            4881355691,
+            2587800407,
+            7581302759,
+            8038703119
         };
-        // will add more primes later
+        bigPrimes[17] = {
+            63343274992169129,
+            47146252877352841,
+            22636999438935137,
+            24922865713067633,
+            81189988142526161,
+            71652070271607377,
+            22797312194041361,
+            89974847561296171,
+            82770485597997697,
+            14173133140153553
+        };
+        bigPrimes[18] = {
+            434962058169494329,
+            770158542645541471,
+            350181419397386131,
+            546353091746733769,
+            711081299063207953,
+            216284664811929913,
+            591097573201855849,
+            184738237144951801,
+            925755983913931259,
+            214159616919506521
+        };
 
-        for (auto it: bigPrimes) sort(it.second.begin(), it.second.end());
+        map<int, vector<ll>>::iterator it = bigPrimes.begin();
+        while (it != bigPrimes.end()) {
+            sort((it->second).begin(), (it->second).end());
+            it++;
+        }
     }
 
     void TestCases() {
@@ -113,29 +136,61 @@ protected:
         }
         
         // large nums   
+        for (int i = 0; i < 5; i++) {
+            CASE(N = randomOdd(1001, 1000000000));
+        }
+
         for (int i = 0; i < 10; i++) {
-            CASE(N = randomOdd(1001, MAXN));
+            CASE(N = randomOdd(1000000001, MAXN));
         }
 
         // primes (1 <= p <= 1e6)
+        for (int i = 0; i < 3; i++) {
+            CASE(N = primes[i + 1]);
+        }
         for (int i = 0; i < 5; i++) {
-            CASE(N = randomPrime(1, LIM));
+            CASE(N = randomPrime(10, 100));
+        }
+        for (int i = 0; i < 5; i++) {
+            CASE(N = randomPrime(101, 1000));
+        }
+        for (int i = 0; i < 5; i++) {
+            CASE(N = randomPrime(1001, LIM));
         }
 
         // large primes
         for (int i = 0; i < 3; i++) {
             CASE(N = randomBigPrime(7));
         }
-        CASE(N = randomBigPrime(17));
-
+        for (int i = 0; i < 3; i++) {
+            CASE(N = randomBigPrime(8));
+        }
+        for (int i = 0; i < 3; i++) {
+            CASE(N = randomBigPrime(9));
+        }
+        for (int i = 0; i < 3; i++) {
+            CASE(N = randomBigPrime(10));
+        }
+        for (int i = 0; i < 10; i++) {
+            CASE(N = bigPrimes[17][i]);
+        }
+        for (int i = 0; i < 10; i++) {
+            CASE(N = bigPrimes[18][i]);
+        }
+        
         // mult
         for (int i = 0; i < 3; i++) {
-            CASE(N = randomBigPrime(7) * randomBigPrime(8));
+            CASE(N = bigPrimes[8][randomID(bigPrimes[7])] * bigPrimes[8][randomID(bigPrimes[8])]);
         }
         for (int i = 0; i < 3; i++) {
-            CASE(N = randomBigPrime(8) * randomBigPrime(8));
+            CASE(N = bigPrimes[8][randomID(bigPrimes[8])] * bigPrimes[9][randomID(bigPrimes[8])]);
         }
-        // will add more later
+        for (int i = 0; i < 3; i++) {
+            CASE(N = bigPrimes[9][randomID(bigPrimes[9])] * bigPrimes[9][randomID(bigPrimes[9])]);
+        }
+
+        // other
+        CASE(N = 1);
 
         // max
         CASE(N = MAXN - 1);
@@ -152,6 +207,10 @@ private:
 
     ll randomOdd(ll lo, ll hi) {
         ll num = rnd.nextLongLong(lo, getLimit(hi));
+        while (vis[2 * num + 1]) {
+            num = rnd.nextLongLong(lo, getLimit(hi));
+        }
+        vis[2 * num + 1] = true;
         return (2*num + 1);
     }
 
@@ -159,13 +218,19 @@ private:
         int st = lower_bound(primes.begin(), primes.end(), lo) - primes.begin();
         int en = (upper_bound(primes.begin(), primes.end(), hi) - primes.begin()) - 1;
         int id = 1;
+        st = max(st, id);
         if (st >= 1 && en < sz_prime) {
             id = rnd.nextInt(st, en);
+            while (vis[primes[id]]) id = rnd.nextInt(st, en);
+            vis[primes[id]] = true;
         }
         return primes[id];
     }
 
     ll randomBigPrime(int digit) {
-        return bigPrimes[digit][randomID(bigPrimes[digit])];
+        ll ret = bigPrimes[digit][randomID(bigPrimes[digit])];
+        while (vis[ret]) ret = bigPrimes[digit][randomID(bigPrimes[digit])];
+        vis[ret] = true;
+        return ret;
     }
 };
