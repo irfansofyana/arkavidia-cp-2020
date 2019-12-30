65b52460cc9e2d706f86275a4cbb1e3bcf2b21cf
Irfan Sofyana Putra <irfansofyana0305@gmail.com>
2019-12-30 12:50:19 +0700

    finalize spec and fix solution-alt

--- arkavidia-2020-longest-arithmetic-subarray/spec.cpp
+++ arkavidia-2020-longest-arithmetic-subarray/spec.cpp
@@ -7,6 +7,7 @@ using namespace std;
 const int MAXN = 100000;
 const int MAXQ = 100000;
 const int MAXB = 1000000000;
+const int MAXD = 10000;
 
 class ProblemSpec : public BaseProblemSpec {
 protected:
@@ -114,21 +115,167 @@ protected:
            R = {3, 3, 2}
        );
 
-        for (int i = 0; i < 3; ++i){
+        for (int i = 0; i < 1; ++i){
             CASE(
                 N = rnd.nextInt(1, 1000),
                 randomAllArithmeticSeqInc(N, arr),
                 Q = rnd.nextInt(1, 1000),
-                randomRange(Q, L, R, N);
+                randomRange(Q, L, R, N)
+            );
+            CASE(
+                N = rnd.nextInt(1, 1000),
+                randomAllArithmeticSeqDec(N, arr),
+                Q = rnd.nextInt(1, 1000),
+                randomRange(Q, L, R, N)
             );
         }
+
+        for (int i = 0; i < 1; ++i){
+            CASE(
+                N = rnd.nextInt(10001, MAXN),
+                randomAllArithmeticSeqInc(N, arr),
+                Q = rnd.nextInt(10001, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+            CASE(
+                N = rnd.nextInt(10001, MAXN),
+                randomAllArithmeticSeqDec(N, arr),
+                Q = rnd.nextInt(10001, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+        }
+
+        for (int i = 0; i < 10; ++i){
+            CASE(
+                N = rnd.nextInt(10001, MAXN),
+                randomPartialArithmeticSeq(N, arr),
+                Q = rnd.nextInt(10001, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+        }
+
+        for (int i = 0; i < 5; ++i){
+            CASE(
+                N = rnd.nextInt(10001, MAXN),
+                randomPartialArithmeticSeqDec(N, arr),
+                Q = rnd.nextInt(10001, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+        }
+
+        for (int i = 0; i < 5; ++i){
+            CASE(
+                N = rnd.nextInt(10001, MAXN),
+                fullRandom(N, arr),
+                Q = rnd.nextInt(10001, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+        }
+
+        for (int i = 0; i < 3; ++i){
+            CASE(
+                N = MAXN, 
+                randomPartialArithmeticSeq(N, arr),
+                Q = rnd.nextInt(1, MAXQ),
+                randomRange(Q, L, R, N)
+            );
+        }
+
+        CASE(
+            N = MAXN,
+            randomAllArithmeticSeqInc(N, arr),
+            Q = MAXQ,
+            randomRange(Q, L, R, N)
+        );
+        CASE(
+            N = MAXN, 
+            randomPartialZeroDiff(N, arr),
+            Q = MAXQ, 
+            randomRange(Q, L, R, N)
+        );
     }
 
 private:
+    pair<int,int> generateNumber(int sz, int mini, int maks, int mind, int maksd){
+        int a = rnd.nextInt(mini, maks);
+        int b = rnd.nextInt(mind, maksd);
+        long long last = 1LL*(a + 1LL * (sz-1) * b);
+        while (last > 1LL*maks || last < 1LL*mini){
+            a = rnd.nextInt(mini, maks);
+            b = rnd.nextInt(mind, maksd);
+            last = 1LL*(a + 1LL * (sz-1) * b);
+        }
+        return make_pair(a, b);
+    }
+
+    void randomPartialArithmeticSeq(int sz, vector<int>& arr){
+        pair<int,int> tmp = generateNumber(sz, -MAXB, MAXB, -MAXD, MAXD);
+        int a = tmp.first;
+        int b = tmp.second;
+        arr.push_back(a);
+        a+=b;
+        arr.push_back(a);
+        a+=b;
+        for (int i = 0; i < sz-2; ++i){
+            int rd = rnd.nextInt(0, 1);
+            if (rd == 0){
+                arr.push_back(a);
+                a += b;
+            }else {
+                arr.push_back(a+1);
+                a = a+1; a += b;
+            }
+        }
+    }
+
+    void randomPartialArithmeticSeqDec(int sz, vector<int>& arr){
+        pair<int,int> tmp = generateNumber(sz, -MAXB, 0, -MAXD, 0);
+        int a = tmp.first;
+        int b = tmp.second;
+        arr.push_back(a);
+        a+=b;
+        arr.push_back(a);
+        a+=b;
+        for (int i = 0; i < sz-2; ++i){
+            int rd = rnd.nextInt(0, 1);
+            if (rd == 0){
+                arr.push_back(a);
+                a += b;
+            }else {
+                arr.push_back(a+1);
+                a = a+1; a += b;
+            }
+        }
+    }
+
     void randomAllArithmeticSeqInc(int sz, vector<int>& arr){
         int a = rnd.nextInt(-MAXB, MAXB);
-        int b = rnd.nextInt()
-        long long last = 1LL*(a + (sz-1) * 
+        int b = rnd.nextInt(0, MAXD);
+        long long last = 1LL*(a + 1LL * (sz-1) * b);
+        while (last > 1ll*MAXB){
+            a = rnd.nextInt(-MAXB, MAXB);
+            b = rnd.nextInt(0, MAXD);
+            last = 1LL*(a + 1LL * (sz-1) * b);
+        }
+        for (int i = 0; i < sz; ++i){
+            arr.push_back(a);
+            a += b;
+        }
+    }
+
+    void randomAllArithmeticSeqDec(int sz, vector<int>& arr){
+        int a = rnd.nextInt(-MAXB, MAXB);
+        int b = rnd.nextInt(-MAXB, 0);
+        long long last = 1LL*(a + 1LL * (sz-1) * b);
+        while (last < 1ll*-MAXB){
+            a = rnd.nextInt(-MAXB, MAXB);
+            b = rnd.nextInt(0, MAXD);
+            last = 1LL*(a + 1LL * (sz-1) * b);
+        }
+        for (int i = 0; i < sz; ++i){
+            arr.push_back(a);
+            a += b;
+        }
     }
 
     void randomRange(int sz, vector<int> & L, vector<int> & R, int maks){
@@ -137,6 +284,40 @@ private:
             int r = rnd.nextInt(l, maks);
             L.push_back(l);
             R.push_back(r);
+        }
+    }
+
+    void randomPartialZeroDiff(int sz, vector<int>& arr){
+        int a = rnd.nextInt(-MAXB, MAXB);
+        arr.push_back(a);
+        for (int i = 1; i < sz; ++i){
+            if (rnd.nextInt(0, 1) == 0){
+                arr.push_back(arr[i-1]);
+            }else {
+                a = rnd.nextInt(-MAXB, MAXB);
+                arr.push_back(a);
+            }
+        }
+    }
+
+    void fullRandom(int sz, vector<int>& arr){
+        pair<int,int> tmp = generateNumber(sz, -MAXB, MAXB, -MAXD, MAXD);
+        int a = tmp.first;
+        int b = tmp.second;
+        arr.push_back(a);
+        a+=b;
+        arr.push_back(a);
+        a+=b;
+        for (int i = 0; i < sz-2; ++i){
+            if (rnd.nextInt(0, 1) == 0){
+                arr.push_back(a);
+                a += b;
+            }else {
+                pair<int,int> tmp = generateNumber(sz, -MAXB, MAXB, -MAXD, MAXD);
+                a = tmp.first;
+                b = tmp.second;
+                arr.push_back(a);
+            }
         }
     }
 };
