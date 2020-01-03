05af324fa6e8c5d302ccddae21f730dd7cad1786
Irfan Sofyana Putra <irfansofyana0305@gmail.com>
2020-01-03 21:44:59 +0700

    change problem

---
+++ arkavidia-2020-qual-trial-soal-mudah/spec.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <tcframe/spec.hpp>
+using namespace std;
+using namespace tcframe;
+
+class ProblemSpec : public BaseProblemSpec {
+protected:
+    int a, b;
+    int ans;
+
+    void InputFormat() {
+        //
+        LINE(a, b)
+    }
+
+    void OutputFormat() {
+        LINE(ans);
+    }
+
+    void GradingConfig() {
+        TimeLimit(1);
+        MemoryLimit(64);
+    }
+
+    void Constraints() {
+        //
+        CASE(1 <= a && a <= 100);
+        CASE(1 <= b && b <= 100);
+    }
+};
+
+class TestSpec : public BaseTestSpec<ProblemSpec> {
+protected:
+    void TestCases() {
+        for (int i = 0; i < 5; ++i){
+            CASE(a = rnd.nextInt(1, 100), b = rnd.nextInt(1, 100));
+        }
+    }
+};
